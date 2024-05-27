d532e41064a77a1ed012c898eb49ae55c263c1a1
JoonYong <joonyongpark99@gmail.com>
2024-05-27 22:28:42 +0900

    initial commit

---
+++ src/main/java/com/flip/flipapp/FlipappApplication.java
@@ -0,0 +1,13 @@
+package com.flip.flipapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlipappApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/flip/flipapp/FlipappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flip.flipapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
