d31174aaaa3d7011a20a40e361427955597d5b7e
hojoon <ujuj0202@gmail.com>
2024-06-07 19:59:58 +0900

    FLIP-125 [FEAT] SecurityConfig 추가

---
+++ src/main/java/com/flip/flipapp/global/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.flip.flipapp.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .sessionManagement(sessionManagement -> sessionManagement
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/error/**", "/actuator/**", "/docs/**").permitAll()
+            .anyRequest().authenticated());
+
+    return http.build();
+  }
+}
