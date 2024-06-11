e2f80505be5ff898846f8cddf59042822424598e
hojoon <ujuj0202@gmail.com>
2024-06-11 23:38:44 +0900

    FLIP-131 [TEST] test 환경 격리를 위한 CleanUpTestExecutionListener 구현
    afterTestMethod 에 의해 테스트마다 끝난뒤 테이블을 TRUNCATE 시킴

---
+++ src/test/java/com/flip/flipapp/common/CleanUpTestExecutionListener.java
@@ -0,0 +1,36 @@
+package com.flip.flipapp.common;
+
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.support.AbstractTestExecutionListener;
+
+public class CleanUpTestExecutionListener extends AbstractTestExecutionListener {
+
+  @Override
+  public void afterTestMethod(final TestContext testContext) {
+    final JdbcTemplate jdbcTemplate = getJdbcTemplate(testContext);
+    final List<String> truncateQueries = getTruncateQueries(jdbcTemplate);
+    truncateTables(jdbcTemplate, truncateQueries);
+  }
+
+  private List<String> getTruncateQueries(final JdbcTemplate jdbcTemplate) {
+    return jdbcTemplate.queryForList(
+        "SELECT Concat('TRUNCATE TABLE ', TABLE_NAME, ';') AS q FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = DATABASE()",
+        String.class);
+  }
+
+  private JdbcTemplate getJdbcTemplate(final TestContext testContext) {
+    return testContext.getApplicationContext().getBean(JdbcTemplate.class);
+  }
+
+  private void truncateTables(final JdbcTemplate jdbcTemplate, final List<String> truncateQueries) {
+    execute(jdbcTemplate, "SET FOREIGN_KEY_CHECKS = 0");
+    truncateQueries.forEach(v -> execute(jdbcTemplate, v));
+    execute(jdbcTemplate, "SET FOREIGN_KEY_CHECKS = 1");
+  }
+
+  private void execute(final JdbcTemplate jdbcTemplate, final String query) {
+    jdbcTemplate.execute(query);
+  }
+}
--- src/test/java/com/flip/flipapp/common/SpringBootTestWithRestDocs.java
+++ src/test/java/com/flip/flipapp/common/SpringBootTestWithRestDocs.java
@@ -7,12 +7,15 @@ import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @AutoConfigureRestDocs
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestExecutionListeners(value = {
+    CleanUpTestExecutionListener.class}, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
 public @interface SpringBootTestWithRestDocs {
 
 }
