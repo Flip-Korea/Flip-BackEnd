5dc16b86acbc03c74a896acd4429e4405ad66093
hojoon <ujuj0202@gmail.com>
2024-06-18 01:07:38 +0900

    FLIP-135 [TEST] 통합테스트코드 작성

---
+++ src/test/resources/com/flip/flipapp/domain/comment/controller/addCommentTest.sql
@@ -0,0 +1,18 @@
+INSERT INTO category
+VALUES (1, 'DAILY');
+INSERT INTO category
+VALUES (2, 'IT_SCIENCE');
+INSERT INTO category
+VALUES (3, 'TIPS');
+INSERT INTO category
+VALUES (4, 'THOUGHTS');
+
+INSERT INTO account (account_id, oauth_id, account_state, recent_login)
+VALUES (1, 'oauth', 'ACTIVE', 1);
+
+INSERT INTO profile (profile_id, user_id, nickname, introduce, account_id)
+VALUES (1, 'user', 'nickname', '자기소개', 1);
+
+INSERT INTO post (post_id, title, content, post_at, bg_color, font_style, like_cnt, profile_id,
+                  category_id)
+VALUES (1, 'title', 'content', now(), 'RED', 'BOLD', 0, 1, 1);
