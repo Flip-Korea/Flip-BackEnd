4c35de72cae32caeae173556b575b3ea3e6bdfbb
hojoon <ujuj0202@gmail.com>
2024-06-19 00:47:26 +0900

    FLIP-138 [FIX] 카테고리 전체 조회 API 접근 변경

--- src/main/java/com/flip/flipapp/global/config/SecurityConfig.java
+++ src/main/java/com/flip/flipapp/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.flip.flipapp.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -32,6 +33,7 @@ public class SecurityConfig {
             .authenticationEntryPoint(customAuthenticationEntryPoint))
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers("/error/**", "/actuator/**", "/docs/**").permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/v1/categories").permitAll()
             .anyRequest().authenticated());
 
     return http.build();
