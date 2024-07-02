8eab2fc5d64a92746d9463c47aa0a9849e7bb4c8
hojoon <ujuj0202@gmail.com>
2024-07-03 00:32:56 +0900

    FLIP-139 [TEST] 테스트코드 P6Spy 적용

--- src/test/java/com/flip/flipapp/domain/interest_category/repository/InterestCategoryRepositoryTest.java
+++ src/test/java/com/flip/flipapp/domain/interest_category/repository/InterestCategoryRepositoryTest.java
@@ -3,15 +3,18 @@ package com.flip.flipapp.domain.interest_category.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.flip.flipapp.domain.interest_category.model.InterestCategory;
+import com.flip.flipapp.global.config.P6SpyConfig;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(P6SpyConfig.class)
 class InterestCategoryRepositoryTest {
 
   @Autowired
