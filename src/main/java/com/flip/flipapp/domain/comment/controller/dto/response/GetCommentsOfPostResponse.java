d6ba9fb643be716b8420307bba2656ae4102bec0
hojoon <ujuj0202@gmail.com>
2024-07-05 22:55:52 +0900

    FLIP-147 [FEAT] 포스트 댓글 조회 API controller 구현

---
+++ src/main/java/com/flip/flipapp/domain/comment/controller/dto/response/GetCommentsOfPostResponse.java
@@ -0,0 +1,39 @@
+package com.flip.flipapp.domain.comment.controller.dto.response;
+
+import com.flip.flipapp.domain.comment.repository.dto.CommentOfPostDto;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.domain.Page;
+
+public record GetCommentsOfPostResponse(
+    List<CommentOfPost> comments,
+    long totalCount
+) {
+
+  private record CommentOfPost(
+      Long commentId,
+      String content,
+      LocalDateTime commentAt,
+      Long profileId,
+      String nickname,
+      String profileImageUrl
+
+  ) {
+
+  }
+
+  public static GetCommentsOfPostResponse from(Page<CommentOfPostDto> page) {
+    List<CommentOfPost> comments = page.getContent().stream()
+                                       .map(content ->
+                                           new CommentOfPost(
+                                               content.getCommentId(),
+                                               content.getContent(),
+                                               content.getCommentAt(),
+                                               content.getProfileId(),
+                                               content.getNickname(),
+                                               content.getProfileImageUrl()
+                                           ))
+                                       .toList();
+    return new GetCommentsOfPostResponse(comments, page.getTotalElements());
+  }
+}
