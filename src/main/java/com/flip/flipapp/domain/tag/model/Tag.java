9a8c19625bce97d2a9f829f79ef5331ccf31c2f0
hojoon <ujuj0202@gmail.com>
2024-07-11 21:33:24 +0900

    FLIP-152 [FEAT] 연관관계명 변경, 빌더 추가

--- src/main/java/com/flip/flipapp/domain/tag/model/Tag.java
+++ src/main/java/com/flip/flipapp/domain/tag/model/Tag.java
@@ -6,13 +6,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tag")
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Tag {
 
   @Id
