70aeb1d9b8b6c300b912620ad2efed3a0840386b
hojoon <ujuj0202@gmail.com>
2024-07-13 23:43:14 +0900

    FLIP-152 [FEAT] errorcode 정의

---
+++ src/main/java/com/flip/flipapp/domain/category/exception/CategoryNotFoundException.java
@@ -0,0 +1,10 @@
+package com.flip.flipapp.domain.category.exception;
+
+import com.flip.flipapp.global.exception.BusinessException;
+
+public class CategoryNotFoundException extends BusinessException {
+
+  public CategoryNotFoundException() {
+    super(CategoryErrorCode.CATEGORY_NOT_FOUND);
+  }
+}
