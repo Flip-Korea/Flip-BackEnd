2fea3006d8c8ab807a262ac69ec1cd9b9bec4a1d
hojoon <ujuj0202@gmail.com>
2024-07-19 15:09:19 +0900

    FLIP-154 [TEST] 테스트코드용 sql

---
+++ src/test/resources/com/flip/flipapp/domain/temp_post/controller/addTempPost.sql
@@ -0,0 +1,14 @@
+INSERT INTO category
+VALUES (1, 'DAILY');
+INSERT INTO category
+VALUES (2, 'IT_SCIENCE');
+INSERT INTO category
+VALUES (3, 'TIPS');
+INSERT INTO category
+VALUES (4, 'THOUGHTS');
+
+INSERT INTO account (account_id, oauth_id, account_state, recent_login)
+VALUES (1, 'oauth', 'ACTIVE', 1);
+
+INSERT INTO profile (profile_id, user_id, nickname, introduce, account_id)
+VALUES (1, 'user', 'nickname', '자기소개', 1);
