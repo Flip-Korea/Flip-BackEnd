d0d9d33d7da6e06ee77f74c3ea748178a56c8037
hojoon <ujuj0202@gmail.com>
2024-07-24 21:15:04 +0900

    FLIP-155 [FEAT] querydsl Custom Repository 추가

--- src/main/java/com/flip/flipapp/domain/temp_post/repository/TempPostRepository.java
+++ src/main/java/com/flip/flipapp/domain/temp_post/repository/TempPostRepository.java
@@ -3,6 +3,6 @@ package com.flip.flipapp.domain.temp_post.repository;
 import com.flip.flipapp.domain.temp_post.model.TempPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TempPostRepository extends JpaRepository<TempPost, Long> {
+public interface TempPostRepository extends JpaRepository<TempPost, Long>, TempPostRepositoryCustom {
 
 }
