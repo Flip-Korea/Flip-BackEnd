81ba1f9667ce970d1e2fcf05748b5a6e5d863e30
JoonYong <joonyongpark99@gmail.com>
2024-08-14 01:08:27 +0900

    FLIP-150 [FEAT] JWT 필터 구현

---
+++ src/main/java/com/flip/flipapp/global/security/jwt/JwtFilter.java
@@ -0,0 +1,80 @@
+package com.flip.flipapp.global.security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flip.flipapp.global.error.ErrorResponse;
+import com.flip.flipapp.global.error.exception.BusinessException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+  private final JwtProvider jwtProvider;
+  private final ObjectMapper objectMapper;
+  private static final String AUTHORIZATION_HEADER = "Authorization";
+  private static final String AUTHORIZATION_TYPE = "Bearer ";
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+    String jwtToken = getJwtFromRequest(request);
+    try {
+      if (StringUtils.hasText(jwtToken)) {
+        jwtProvider.validateToken(jwtToken);
+        String username = jwtProvider.getSubjectFromToken(jwtToken);
+        UserDetails userDetails = getUserDetails(username);
+        Authentication authentication = getAuthentication(userDetails);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+      }
+      filterChain.doFilter(request, response);
+    } catch (BusinessException e) {
+      handleException(response, e);
+    }
+  }
+
+  private String getJwtFromRequest(HttpServletRequest request) {
+    String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+    if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(AUTHORIZATION_TYPE)) {
+      return bearerToken.substring(AUTHORIZATION_TYPE.length());
+    }
+    return null;
+  }
+
+  private Authentication getAuthentication(UserDetails userDetails) {
+    return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+  }
+
+  private UserDetails getUserDetails(String accountId) {
+
+    return User.builder()
+        .username(accountId)
+        .password("")
+        .authorities("ROLE_USER")
+        .build();
+  }
+
+  private void handleException(HttpServletResponse response, BusinessException e)
+      throws IOException {
+    response.setStatus(e.getErrorCode().getStatus());
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().write(objectMapper.writeValueAsString(
+        ErrorResponse.of(e.getErrorCode())
+    ));
+  }
+}
+
