4180194e98163f644529e57d7719aa61601b8068
JoonYong <joonyongpark99@gmail.com>
2024-08-17 05:02:48 +0900

    FLIP-150 [REFACTOR] 패키지 경로 수정

--- src/main/java/com/flip/flipapp/domain/follow/exception/SelfFollowException.java
+++ src/main/java/com/flip/flipapp/domain/follow/exception/SelfFollowException.java
@@ -1,6 +1,6 @@
 package com.flip.flipapp.domain.follow.exception;
 
-import com.flip.flipapp.global.exception.BusinessException;
+import com.flip.flipapp.global.error.exception.BusinessException;
 
 public class SelfFollowException extends BusinessException {
 
