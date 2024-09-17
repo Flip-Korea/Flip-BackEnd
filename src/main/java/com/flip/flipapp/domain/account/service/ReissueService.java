8a7c22663858074f5918e8a7438556372d405820
JoonYong <joonyongpark99@gmail.com>
2024-09-17 15:28:03 +0900

    FLIP-200 [REFACTOR] 재발급 API 코드 수정

--- src/main/java/com/flip/flipapp/domain/account/service/ReissueService.java
+++ src/main/java/com/flip/flipapp/domain/account/service/ReissueService.java
@@ -1,11 +1,9 @@
 package com.flip.flipapp.domain.account.service;
 
-import com.flip.flipapp.domain.account.controller.dto.response.JwtResponse;
 import com.flip.flipapp.domain.account.exception.AccountNotFoundException;
 import com.flip.flipapp.domain.account.exception.AccountSuspendedException;
 import com.flip.flipapp.domain.account.model.Account;
 import com.flip.flipapp.domain.account.repository.AccountRepository;
-import com.flip.flipapp.global.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +11,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReissueService {
 
-  private final JwtProvider jwtProvider;
   private final AccountRepository accountRepository;
 
-  public JwtResponse reissue(Long profileId) {
+  public Long reissue(Long profileId) {
     Account account = accountRepository.findById(profileId)
         .orElseThrow(AccountNotFoundException::new);
 
@@ -24,10 +21,7 @@ public class ReissueService {
       throw new AccountSuspendedException();
     }
 
-    String accessToken = jwtProvider.createAccessToken(profileId);
-    String refreshToken = jwtProvider.createRefreshToken(profileId);
-
-    return new JwtResponse(accessToken, refreshToken);
+    return profileId;
   }
 
 }
