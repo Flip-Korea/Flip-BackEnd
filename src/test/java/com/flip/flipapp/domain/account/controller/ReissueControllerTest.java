cab8d0892c65b664be16c488f5ea3785734a0851
JoonYong <joonyongpark99@gmail.com>
2024-09-17 15:31:38 +0900

    FLIP-200 [TEST] 재발급 API 테스트코드 수정

--- src/test/java/com/flip/flipapp/domain/account/controller/ReissueControllerTest.java
+++ src/test/java/com/flip/flipapp/domain/account/controller/ReissueControllerTest.java
@@ -17,6 +17,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flip.flipapp.common.HeaderConstants;
 import com.flip.flipapp.common.SpringBootTestWithRestDocs;
 import com.flip.flipapp.domain.account.exception.AccountSuspendedException;
+import com.flip.flipapp.global.error.exception.CustomMalformedJwtException;
+import com.flip.flipapp.global.security.jwt.JwtProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,15 +37,20 @@ class ReissueControllerTest {
   @Autowired
   ObjectMapper objectMapper;
 
+  @Autowired
+  private JwtProvider jwtProvider;
 
   @Test
   @WithMockUser(username = "1")
   @DisplayName("유효한 요청 시 JWT 토큰을 재발급한다")
   void should_reissue_jwt_tokens_when_request_is_valid() throws Exception {
 
+    String validRefreshToken = jwtProvider.createRefreshToken(1L);
+
     mockMvc.perform(
             post("/api/v1/reissue")
-                .header(HeaderConstants.REFRESH_TOKEN.getKey(), HeaderConstants.REFRESH_TOKEN.getValue())
+                .header(HeaderConstants.REFRESH_TOKEN.getKey(),
+                    "Bearer " + validRefreshToken)
                 .contentType(APPLICATION_JSON)
         )
         .andExpect(status().isOk())
@@ -68,10 +75,14 @@ class ReissueControllerTest {
 
   @Test
   @WithMockUser(username = "2")
-  @DisplayName("정지된 계정으로 토큰 재발급 요청 시 403 응답을 반환한다")
+  @DisplayName("정지된 계정의 JWT 재발급 요청 시 403 오류 반환")
   void should_return_403_when_account_is_suspended() throws Exception {
+
+    String validRefreshToken = jwtProvider.createRefreshToken(2L);
+
     mockMvc.perform(post("/api/v1/reissue")
-            .header(HeaderConstants.ACCESS_TOKEN.getKey(), HeaderConstants.ACCESS_TOKEN.getValue())
+            .header(HeaderConstants.ACCESS_TOKEN.getKey(),
+                "Bearer " + validRefreshToken)
             .contentType(APPLICATION_JSON)
         )
         .andExpectAll(
@@ -80,4 +91,25 @@ class ReissueControllerTest {
                 .isInstanceOf(AccountSuspendedException.class)
         );
   }
+
+  @Test
+  @WithMockUser(username = "1")
+  @DisplayName("리프레쉬 토큰이 아닌 액세스 토큰으로 재발급 요청 시 400 응답을 반환한다")
+  void should_return_400_when_access_token_is_used_instead_of_refresh_token() throws Exception {
+
+    String validAccessToken = jwtProvider.createAccessToken(1L);
+
+
+    mockMvc.perform(post("/api/v1/reissue")
+            .header(HeaderConstants.ACCESS_TOKEN.getKey(),
+                "Bearer " + validAccessToken)
+            .contentType(APPLICATION_JSON)
+        )
+        .andExpectAll(
+            status().isUnauthorized(),
+            result -> assertThat(result.getResolvedException())
+                .isInstanceOf(CustomMalformedJwtException.class)
+        );
+  }
+
 }
