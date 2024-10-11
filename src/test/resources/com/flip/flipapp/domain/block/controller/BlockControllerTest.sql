89e74522b97c0608d69017213c463ad1ea766446
JoonYong <joonyongpark99@gmail.com>
2024-10-11 14:32:03 +0900

    FLIP-206 [TEST] 차단 테스트코드 작성

---
+++ src/test/resources/com/flip/flipapp/domain/block/controller/BlockControllerTest.sql
@@ -0,0 +1,20 @@
+INSERT INTO account (account_id, oauth_id, account_state, recent_login, suspended_at)
+VALUES (1, 'kakaooauth1', 'ACTIVE', 1, null);
+
+INSERT INTO account (account_id, oauth_id, account_state, recent_login, suspended_at)
+VALUES (2, 'kakaooauth2', 'ACTIVE', 1, null);
+
+INSERT INTO account (account_id, oauth_id, account_state, recent_login, suspended_at)
+VALUES (3, 'kakaooauth3', 'ACTIVE', 1, null);
+
+INSERT INTO profile (profile_id, user_id, nickname, introduce, account_id)
+VALUES (1, 'user1', 'nickname1', '자기소개1', 1);
+
+INSERT INTO profile (profile_id, user_id, nickname, introduce, account_id)
+VALUES (2, 'user2', 'nickname2', '자기소개2', 2);
+
+INSERT INTO profile (profile_id, user_id, nickname, introduce, account_id)
+VALUES (3, 'user3', 'nickname3', '자기소개3', 3);
+
+INSERT INTO block (block_id, blocker_id, blocked_id)
+VALUES (1, 1, 3);
