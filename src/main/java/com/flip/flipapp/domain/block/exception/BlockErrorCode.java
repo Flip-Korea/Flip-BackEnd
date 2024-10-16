069d8f4e3da71e19fa917a965cec3aad55ceff70
JoonYong <joonyongpark99@gmail.com>
2024-10-17 08:15:11 +0900

    FLIP-208 [FEAT] 예외 메시지 수정

--- src/main/java/com/flip/flipapp/domain/block/exception/BlockErrorCode.java
+++ src/main/java/com/flip/flipapp/domain/block/exception/BlockErrorCode.java
@@ -8,7 +8,7 @@ public enum BlockErrorCode implements ErrorCode {
 
   DUPLICATED_BLOCK("B001", "이미 차단한 유저입니다.", 400),
   SELF_BLOCK("B002", "자기 자신을 차단할 수 없습니다.", 400),
-  BLOCK_NOT_FOUND("B003", "차단한 유저가 존재하지 않습니다.", 400),
+  BLOCK_NOT_FOUND("B003", "해당 차단 내역이 존재하지 않습니다.", 400),
   NOT_BLOCKER("B004", "차단자가 아닙니다.", 400);
 
   private final String code;
