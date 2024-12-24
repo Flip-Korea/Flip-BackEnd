cc87d4568e60c265309eab35b50fe98e6204b92f
JoonYong <joonyongpark99@gmail.com>
2024-12-25 05:47:29 +0900

    FS-11 [TEST] 테스트코드 수정

--- src/test/resources/com/flip/flipapp/domain/account/controller/RegisterControllerTest.sql
+++ src/test/resources/com/flip/flipapp/domain/account/controller/RegisterControllerTest.sql
@@ -1,12 +1,5 @@
-INSERT INTO category
-VALUES (1, 'DAILY');
-INSERT INTO category
-VALUES (2, 'IT_SCIENCE');
-INSERT INTO category
-VALUES (3, 'TIPS');
-INSERT INTO category
-VALUES (4, 'THOUGHTS');
-INSERT INTO category
-VALUES (5, 'HI');
-INSERT INTO category
-VALUES (6, 'HI2');
+INSERT INTO account (account_id, oauth_id, account_state, recent_login, suspended_at, ads_agree)
+VALUES (1, 'kakaooauth456', 'ACTIVE', 1, null, true);
+
+INSERT INTO profile (profile_id, user_id, nickname, introduce, account_id, following_cnt, follower_cnt, post_cnt)
+VALUES (1, 'user456', 'nickname456', '자기소개1', 1, 0, 0, 0);
